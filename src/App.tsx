3c14e0e44881f21959f4dc9852d7208dd12b9038
jimmy_kwon <kyh@neighbor.co.kr>
2023-11-14 12:52:08 +0900

    로컬 스토리지 사용량 확인 대략적인 값 표출 ( 브라우저마다 다를수있습니다. ) 대략 5메가 하단 동작 로그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect, MouseEvent} from 'react';
+import React, { useState, useEffect, useRef, MouseEvent } from 'react';
 import './App.css';
 
+// Shape 인터페이스: 도형의 속성을 정의합니다 (ID, 순서, 위치, 크기, 타입).
 interface Shape {
     id: number;
     order: number;
@@ -12,6 +13,7 @@ interface Shape {
 }
 
 function App() {
+    // 상태 관리: 도형 목록, 현재 도형, 모드, 선택된 도형 ID, 드래그 여부, 로그 등을 관리합니다.
     const [shapes, setShapes] = useState<Shape[]>(() => {
         // localStorage에서 도형 데이터 불러오기
         const savedShapes = localStorage.getItem('shapes');
@@ -19,34 +21,66 @@ function App() {
     });
     const [currentShape, setCurrentShape] = useState<Shape | null>(null);
     const [mode, setMode] = useState<'rectangle' | 'circle' | 'delete' | 'move' | null>(null);
-    const [isDragging, setIsDragging] = useState(false);
     const [selectedShapeId, setSelectedShapeId] = useState<number | null>(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [logs, setLogs] = useState<string[]>([]);
+    const logsEndRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         // shapes 상태가 변경될 때마다 localStorage에 저장
         localStorage.setItem('shapes', JSON.stringify(shapes));
     }, [shapes]);
 
+    useEffect(() => {
+        logsEndRef.current?.scrollIntoView({behavior: 'smooth'});
+    }, [logs]);
+
+    // 로그 추가 함수: 애플리케이션의 상태 변화를 로그로 기록합니다.
+    const addLog = (message: string) => {
+        setLogs(prevLogs => [...prevLogs, `Log ${prevLogs.length + 1}: ${message}`]);
+    };
+
+    // 그리기 모드 설정 함수: 사각형 또는 원 그리기 모드를 설정합니다.
     const startDrawing = (drawingMode: 'rectangle' | 'circle') => {
         setMode(drawingMode);
         setCurrentShape(null);
-        setSelectedShapeId(null);
+        setSelectedShapeId(null); // 선택된 도형 ID 초기화
+        addLog(`${drawingMode} 그리기 모드 시작`);
     };
 
+    // 삭제 모드 설정 함수: 도형을 삭제하는 모드를 설정합니다.
     const setDeleteMode = () => {
         setMode('delete');
         setCurrentShape(null);
-        setSelectedShapeId(null);
+        setSelectedShapeId(null); // 선택된 도형 ID 초기화
+        addLog('선택 지우기 모드 시작');
     };
 
+    // 이동 모드 설정 함수: 도형을 이동하는 모드를 설정합니다.
     const setMoveMode = () => {
         setMode('move');
         setCurrentShape(null);
+        addLog('도형 이동 모드 시작');
     };
 
+    // 모든 도형 삭제 함수: 도형 목록을 비웁니다.
     const clearAllShapes = () => {
         setShapes([]);
+        addLog('모든 도형 삭제');
     };
+
+    // 특정 타입의 도형 개수를 세는 함수
+    const countShapes = (type: 'rectangle' | 'circle') => {
+        return shapes.filter(shape => shape.type === type).length;
+    };
+
+    // 로컬 스토리지 사용량을 추정하는 함수
+    const estimateLocalStorageUsage = () => {
+        const total = encodeURIComponent(JSON.stringify(localStorage)).length;
+        const maxStorage = 5 * 1024 * 1024; // 대략 5MB
+        return `현재 로컬 스토리지 사용량: ${total} bytes, 남은 용량: ${maxStorage - total} bytes`;
+    };
+
 
     const bringToFront = () => {
         if (selectedShapeId !== null) {
@@ -177,6 +211,15 @@ function App() {
                         borderRadius: currentShape.type === 'circle' ? '50%' : '0',
                     }}/>
                 )}
+            </div>
+            <div className="shape-info">
+                <p>사각형 갯수: {countShapes('rectangle')}</p>
+                <p>원 갯수: {countShapes('circle')}</p>
+                <p>{estimateLocalStorageUsage()}</p>
+            </div>
+            <div className="logs">
+                {logs.map((log, index) => <div key={index}>{log}</div>)}
+                <div ref={logsEndRef}/>
             </div>
         </div>
     );
