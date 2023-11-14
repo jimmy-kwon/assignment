b1472adf4bb81e376d3a2f3e4e4a58c76b40a98b
jimmy_kwon <kyh@neighbor.co.kr>
2023-11-14 11:35:42 +0900

    사각형 그리기
    Shape 인터페이스: 사각형을 정의하는 데 필요한 기본 속성들을 포함합니다.
    초기 상태 설정 및 useState 훅 사용: 사각형의 목록을 저장하고 관리합니다.
    드래그 이벤트 핸들러: 마우스 드래그를 통해 사각형을 그리는 기능을 구현합니다.
    사각형 그리기 모드 설정 함수: 사용자가 사각형 그리기 모드로 전환할 수 있도록 합니다.
    사각형 렌더링: 그려진 사각형을 화면에 표시합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, MouseEvent } from 'react';
 import './App.css';
 
+interface Shape {
+  id: number;
+  order: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  type: 'rectangle' | 'circle';
+}
+
 function App() {
+  const [shapes, setShapes] = useState<Shape[]>([]);
+  const [currentShape, setCurrentShape] = useState<Shape | null>(null);
+  const [mode, setMode] = useState<'rectangle' | 'circle' | 'delete' | 'move' | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const startDrawing = (drawingMode: 'rectangle' | 'circle') => {
+    setMode(drawingMode);
+    setCurrentShape(null);
+  };
+
+  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    if (mode === 'rectangle') {
+      const newShape = { id: Date.now(), order: shapes.length + 1, x, y, width: 0, height: 0, type: mode };
+      setCurrentShape(newShape);
+      setIsDragging(true);
+    }
+  };
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (!isDragging || mode !== 'rectangle') return;
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    if (currentShape) {
+      setCurrentShape({...currentShape, width: x - currentShape.x, height: y - currentShape.y});
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (isDragging && currentShape && mode === 'rectangle') {
+      setShapes([...shapes, currentShape]);
+      setCurrentShape(null);
+    }
+    setIsDragging(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App">
+        <div className="toolbar">
+          <button className="simple-button" onClick={() => startDrawing('rectangle')}>사각형 그리기</button>
+        </div>
+        <div
+            className="drawing-area"
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {shapes.map((shape, index) => (
+              <div
+                  key={index}
+                  style={{
+                    position: 'absolute',
+                    left: shape.x,
+                    top: shape.y,
+                    width: Math.abs(shape.width),
+                    height: Math.abs(shape.height),
+                    border: '2px solid black',
+                  }}
+              />
+          ))}
+          {currentShape && (
+              <div style={{
+                position: 'absolute',
+                left: currentShape.x,
+                top: currentShape.y,
+                width: Math.abs(currentShape.width),
+                height: Math.abs(currentShape.height),
+                backgroundColor: 'rgba(0,0,0,0.1)',
+                border: '2px dashed grey',
+              }}/>
+          )}
+        </div>
+      </div>
   );
 }
 
